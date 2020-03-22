539b18be532acc7253fc532ce5f55ef0d9a1ec06
Lukas Haring <lukasharing@hotmail.com>
2020-03-22 01:19:42 +0100

    Camera Optimization Based on a single Vector

--- js/camera.js
+++ js/camera.js
@@ -4,19 +4,14 @@ class Camera{
   constructor(_game){
     this.game = _game;
 
-    // Transition
-    this.transformation_speed = 2.;
-
-    // Position
     this.lookingAt = null;
-    this.from_position = new Vector();
-    this.position = new Vector();
-    this.translation_time = 0.0;
 
-    // Rotation
-    this.rotation_time = 0.0;
-    this.from_rotation = 0.0;
-    this.current_rotation = 0.0;
+    // Transformation Vec3 ( Position[x,y] , Rotation )
+    this.from_transformation = new Vector();
+    this.transformation = new Vector();
+    this.transformation_speed = 2.5;
+    this.transformation_time = 0.0;
+    
     this.camera_hsize = new Vector();
   };
 
@@ -24,64 +19,41 @@ class Camera{
     this.camera_hsize = new Vector(this.game.width >> 1, this.game.height >> 1);
   };
 
-  set rotation(a){ this.current_rotation = a; };
-  get rotation(){ return this.current_rotation; };
+  get position(){ return this.transformation.xy; };
+  get rotation(){ return this.transformation.z; };
 
   lookAt(_entity){
     this.lookingAt = _entity;
-    this.position = _entity.position;
-    this.rotation = _entity.rotation;
+    this.transformation = new Vector(_entity.position.x, _entity.position.y, _entity.rotation);
   };
 
-  // TODO Vec3(pos, angle)
   update(dt){
 
-    // Traslate Camera
-    let dv = this.lookingAt.position.subtract(this.from_position);
-    let dvl = dv.length;
-    if(dvl > EPSILON){
-      this.translation_time += 0.1;
-      let speed = dvl < 10.0 ? this.translation_time : this.transformation_speed;
-
-      let t = this.translation_time / speed;
-      // Camera Interpolation
-      //t = t * t * (3 - 2 * t);
-
-      this.position = this.from_position.mix(this.lookingAt.position, t);
+    const ctransformation = new Vector(this.lookingAt.position.x, this.lookingAt.position.y, this.lookingAt.rotation);
+    const dtransformation = ctransformation.subtract(this.from_transformation);
     
-      if(t >= 1.0){
-        this.from_position = this.position;
-        this.translation_time = 0.0;
-      }
-    }
-
-    // Rotate Camerad
-    let from = this.from_rotation;
-    let to = this.lookingAt.rotation;
     // Check If there is a shortes path (from the other side)
-    const dr = from - to;
-    if(Math.abs(dr) > PI){
-      to += Math.sign(dr) * TAU;
+    if(Math.abs(dtransformation.z) > PI){
+      dtransformation.z -= Math.sign(dtransformation.z) * TAU;
     }
 
-    if(Math.abs(dr) > EPSILON){
-      this.rotation_time += 0.1;
+    let dvl = dtransformation.length;
+    if(dvl > EPSILON){
+      this.transformation_time += 0.1;
 
-      let speed = Math.abs(from - to) < 0.1 ? this.rotation_time : this.transformation_speed;
+      let speed = dvl < 1.0 ? this.transformation_time : this.transformation_speed;
 
-      let t = this.rotation_time / speed;
-
+      let t = this.transformation_time / speed;
       // Camera Interpolation
       t = t * t * (3 - 2 * t);
 
-      this.current_rotation = positive_radians(mix(from, to, t));
-
+      this.transformation = this.from_transformation.add(dtransformation.scale(t));
+    
       if(t >= 1.0){
-        this.from_rotation = this.current_rotation;
-        this.rotation_time = 0.0;
+        this.from_transformation = this.transformation;
+        this.transformation_time = 0.0;
       }
     }
- 
   };
 
   draw_gui(ctx){
